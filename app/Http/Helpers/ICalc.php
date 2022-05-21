7aad40ba349679344ed7c1c862fd43c8cdfddb95
Dmitriy Arshinchenko <dmitriya@bookmap.com>
2022-05-21 11:28:24 +0400

    operation classes

---
+++ app/Http/Helpers/ICalc.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Helpers;
+/**
+ * Interface for operation of calculator
+ */
+
+interface ICalc
+{
+    /**
+     * @param $input1
+     * @param $input2
+     * @return float|integer
+     */
+
+    public function calculate($input1,$input2);
+
+    /**
+     * Fuction return sign of operation
+     * @return string
+     */
+    public function getSign();
+}
---
+++ app/Http/Helpers/MultiplyCalc.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Helpers;
+/**
+ * Divide class
+ */
+class MultiplyCalc implements ICalc
+{
+    /**
+     * Var of  sign
+     * @var string
+     */
+    private $sign = '*';
+
+    /**
+     *  Function to calculate
+     * @param float $input1
+     * @param float $input2
+     * @return float|integer
+     */
+    public function calculate($input1, $input2)
+    {
+        return $input1 * $input2;
+    }
+
+    /**
+     * Function that get the sign
+     * @return string
+     */
+    public function getSign()
+    {
+        return $this->sign;
+    }
+}
